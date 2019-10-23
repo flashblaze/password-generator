dd64514382e2eb9f8ca9442be7b9d18faa3e6766
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2019-10-23 22:07:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Placeholder from './components/Placeholder/Placeholder';
+
+import './App.less';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Placeholder />
+    </div>
+  );
+};
+
+export default App;
