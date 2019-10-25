3662b662f587229dc2df1f21a856dae40f08776d
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2019-10-25 16:20:47 +0530

    Added redux in package.json
    Also displaying current signed in user's info in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
 
 import PasswordConfig from './components/PasswordConfig/PasswordConfig';
+import { auth } from './firebase/firebase.utils';
+// import { setCurrentUser } from './redux/actions/user-action';
 
 import './App.less';
 
 const App = () => {
+  const [currentUser, setCurrentUser] = useState('');
+  useEffect(() => {
+    const unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+    });
+    console.log(currentUser);
+    return function unsSub() {
+      unsubscribeFromAuth();
+    };
+  });
   return (
     <div className="App">
       <PasswordConfig />
